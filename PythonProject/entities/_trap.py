453db5b3b814bec0b2465f702683edacc285cc2b
Jakub Sadkiewicz <jsadkiewicz@student.agh.edu.pl>
2024-04-23 13:27:17 +0200

    __init__.py added

--- PythonProject/entities/_trap.py
+++ PythonProject/entities/_trap.py
@@ -3,6 +3,3 @@ from entities import Entity
 class Trap(Entity):
     def __init__(self,initiative,position,list_of_moves,max_health,direction,weapon):
         super().__init__(self,initiative,position,list_of_moves,max_health,direction,weapon)
-
-    def deal_damage(self):
-        super().deal_damage()
