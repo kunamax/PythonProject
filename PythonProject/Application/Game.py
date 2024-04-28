9e995faa8acfcd21cb7e36256c5766800aff5d7b
Jakub Sadkiewicz <jsadkiewicz@student.agh.edu.pl>
2024-04-28 17:08:58 +0200

    UI level, jest labirynt, ludek moze chodzic na razie zrobilem na strzalkach jest kolizja

--- PythonProject/Application/Game.py
+++ PythonProject/Application/Game.py
@@ -14,22 +14,29 @@ from Map.HeroOnMap import HeroOnMap
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((1000, 600))
         self.clock = pygame.time.Clock()
         self.scale = 100
         self.running = True
+        self.paused = False
         self.hero = None
 
-        self.create_button = Button(50, 50, 200, 50, "Create Character")
-        self.equip_weapon_button = Button(50, 110, 200, 50, "Equip Weapon")
-        self.equip_armor_button = Button(50, 170, 200, 50, "Equip Armor")
-        self.character_text = Text(50, 230, "")
+        self.wall_texture = pygame.image.load("Resources/wall.jpg")
+        self.floor_texture = pygame.image.load("Resources/floor.jpg")
+        self.hero_texture = pygame.image.load("Resources/hero.png")
+        self.wall_texture = pygame.transform.scale(self.wall_texture, (self.scale, self.scale))
+        self.floor_texture = pygame.transform.scale(self.floor_texture, (self.scale, self.scale))
+        self.hero_texture = pygame.transform.scale(self.hero_texture, (self.scale, self.scale))
+
+        self.pause_button = Button(800, 0, 200, 100, "Pause Game")
+        self.quit_button = Button(800, 100, 200, 100, "Quit Game")
+
         self.game_engine = GameEngine()
 
     def run(self):
         weapon = Weapon("Sword", "A sharp blade", 10, 5)
         armor = Armor("Shield", "A sturdy shield", 15, 3)
-        position = (0, 0)
+        position = (0, 3)
         list_of_moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         max_health = 100
         direction = "right"
@@ -50,36 +57,35 @@ class Game:
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if self.create_button.is_clicked(event):
-                    weapon = Weapon("Sword", "A sharp blade", 10, 5)
-                    armor = Armor("Shield", "A sturdy shield", 15, 3)
-                    position = (0, 0)
-                    list_of_moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-                    max_health = 100
-                    direction = "right"
-                    self.create_character("Hero", 1, weapon, armor, position, list_of_moves, max_health, direction)
-                    self.character_text.text = weapon.name + " and " + armor.name + " equipped"
-                elif self.equip_weapon_button.is_clicked(event):
-                    weapon = Weapon("Axe", "A heavy axe", 15, 7)
-                    self.equip_weapon(weapon)
-                    self.character_text.text = "Weapon equipped"
-                elif self.equip_armor_button.is_clicked(event):
-                    armor = Armor("Plate", "A sturdy plate armor", 20, 5)
-                    self.equip_armor(armor)
-                    self.character_text.text = "Armor equipped"
+                if self.pause_button.is_clicked(event):
+                    if self.paused:
+                        self.paused = False
+                    else:
+                        self.paused = True
+                elif self.quit_button.is_clicked(event):
+                    pygame.quit()
+                    quit()
+            elif event.type == pygame.KEYDOWN:
+                old_position = self.hero.position
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    self.hero.position = (self.hero.position[0], self.hero.position[1] - 1)
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    self.hero.position = (self.hero.position[0], self.hero.position[1] + 1)
+                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    self.hero.position = (self.hero.position[0] - 1, self.hero.position[1])
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    self.hero.position = (self.hero.position[0] + 1, self.hero.position[1])
+                if not self.game_engine.update_map(old_position, self.hero.position):
+                    self.hero.position = old_position
+                print(self.hero.position)
 
     def update(self):
         pass
 
     def draw(self):
-        # self.screen.fill((0, 0, 0))
         self.draw_map()
-        # self.create_button.draw(self.screen)
-        # self.equip_weapon_button.draw(self.screen)
-        # self.equip_armor_button.draw(self.screen)
-        # self.character_text.draw(self.screen)
-        # if self.hero:
-        #     self.hero.draw(self.screen)
+        self.pause_button.draw(self.screen)
+        self.quit_button.draw(self.screen)
         pygame.display.flip()
 
     def draw_map(self):
@@ -87,11 +93,11 @@ class Game:
         for position, tile in map.items():
             x, y = position
             if isinstance(tile, Wall):
-                pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(x * self.scale, y * self.scale, self.scale, self.scale))
+                self.screen.blit(self.wall_texture, (x*self.scale, y*self.scale))
             elif isinstance(tile, Floor):
-                pygame.draw.rect(self.screen, (0, 50, 0), pygame.Rect(x * self.scale, y * self.scale, self.scale, self.scale))
+                self.screen.blit(self.floor_texture, (x*self.scale, y*self.scale))
             elif isinstance(tile, HeroOnMap):
-                pygame.draw.circle(self.screen, (255, 0, 0), (x * self.scale + self.scale // 2, y * self.scale + self.scale // 2), self.scale // 2)
+                self.screen.blit(self.hero_texture, (x*self.scale, y*self.scale))
 
     def add_item(self, item):
         if self.hero:
--- PythonProject/Application/GameEngine.py
+++ PythonProject/Application/GameEngine.py
@@ -8,7 +8,7 @@ import pygame
 class GameEngine:
     def __init__(self):
         self.map = {}
-        self.hero_position = (4, 3)
+        self.hero_position = (0, 3)
         for x in range(8):
             for y in range(6):
                 if random.random() < 0.2 or x == 0 or x == 7 or y == 0 or y == 7:
@@ -29,4 +29,13 @@ class GameEngine:
 
     def set_hero_position(self, new_position):
         self.hero_position = new_position
+
+    def update_map(self, old_position, new_position):
+        if old_position in self.map and isinstance(self.map[old_position], HeroOnMap) and new_position in self.map and not isinstance(self.map[new_position], Wall):
+            self.map[old_position] = Floor()
+            self.map[new_position] = HeroOnMap(new_position)
+            self.hero_position = new_position
+            return True
+        else:
+            return False
 
