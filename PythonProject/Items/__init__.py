5d013cbbb8d5892381c79a20d43e5f675b94b275
SzymonWojturski <szykemcos137@gmail.com>
2024-06-09 00:06:34 +0200

    map generation in engine

--- PythonProject/Items/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._item import Item
-from ._weapon import Weapon
-from ._armor import Armor
-from ._healingPotion import  HealingPotion
-from ._shopItem import ShopItem
-from ._card import Card
-from ._deck import Deck
-print("inicjalisuje modol "+__name__)
--- PythonProject/Items/_card.py
+++
@@ -1,8 +0,0 @@
-from Application.Map._wall import Wall
-
-class Card:
-    def __init__(self, wall: Wall):
-        self.wall = wall
-
-    def __str__(self):
-        return f"Card(wall={self.wall})"
